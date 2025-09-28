eec6492ade134df9bbd9561f67279e40f5edfb1e
Quesia Silva <quesiavirginia2806@gmail.com>
2025-09-28 14:12:18 -0300

    first commit

---
+++ src/main/java/first/project/java/api/controller/ApiController.java
@@ -0,0 +1,41 @@
+package first.project.java.api.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.coyote.Response;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ApiController {
+    private List<String> tasks = new ArrayList<>();
+
+    private ObjectMapper objectMapper;
+
+    public ApiController(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @GetMapping(path = "/tasks")
+    public ResponseEntity<String> listTasks() throws JsonProcessingException {
+        return  ResponseEntity.ok(objectMapper.writeValueAsString(tasks));
+    }
+
+    @PostMapping(path = "/tasks")
+    public ResponseEntity<String> createTask(@RequestBody String task){
+        String cleanTask = task.replace("\"", "");
+        tasks.add(cleanTask);
+        return ResponseEntity.ok("Task: " +cleanTask+" was added succesfully");
+    }
+
+    @DeleteMapping(path = "/tasks")
+    public ResponseEntity<String> clearTaksk(){
+        tasks = new ArrayList<>();
+        return ResponseEntity.ok().build();
+    }
+
+}
